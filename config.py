2ed09658a9fa5fb1537fe5141c4e139f158fecd2
nikolozi2001 <mr.qachibaia44@gmail.com>
2023-08-16 14:08:43 +0400

    added navigation items from list, jinja lesson 8

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+navigation_items = [
+    {"label": "მთავარი", "url": "/"},
+    {"label": "ჩვენ შესახებ", "url": "/about"},
+    {"label": "დამნაშავეები", "url": "/criminals"},
+    {"label": "შესვლა", "url": "#"},
+    {"label": "რეგისტრაცია", "url": "#"}
+]
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
+from config import navigation_items
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    return render_template("index.html", navigation_items=navigation_items)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
-
+    return render_template("about.html", navigation_items=navigation_items)
 
 @app.route("/criminals")
 def criminals():
-    return render_template("criminals.html")
+    return render_template("criminals.html", navigation_items=navigation_items)
+
+if __name__ == "__main__":
+    app.run(debug=True)
